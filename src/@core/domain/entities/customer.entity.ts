28706fa0860189fd1af0c4c642d6660d5410d52a
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2025-04-14 23:15:31 -0300

    feat: aula - criando primeira entidade

--- src/@core/domain/entities/customer.entity.ts
+++ src/@core/domain/entities/customer.entity.ts
@@ -1,5 +1,28 @@
-export class Customer {
-  id: string;
+export type CustomerConstructorProps = {
+  id?: string;
   cpf: string;
   name: string;
+};
+export class Customer {
+  id?: string;
+  cpf: string;
+  name: string;
+
+  constructor(props: CustomerConstructorProps) {
+    this.id = props.id;
+    this.cpf = props.cpf;
+    this.name = props.name;
+  }
+
+  static create(command: { name: string; cpf: string }) {
+    return new Customer(command);
+  }
+
+  toJSON() {
+    return {
+      id: this.id,
+      cpf: this.cpf,
+      name: this.name,
+    };
+  }
 }
